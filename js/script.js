446ae5cb797dbebacdb4e7f69b9ab073a5d09d1c
Goetz <matheusgoetz@gmail.com>
2025-02-19 20:09:49 -0300

    Finalizado js e jquery da atividade

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-//efeito do botão voltar ao Topo
+function topo(){
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  });
+}
 
-//Validação de Login
+function login(){
+  let logado = 0;
+  let usuario = document.getElementById("usuario").value;
+  let senha = document.getElementById("senha").value;
 
-//Ativar alert no botão cadastrar
+  if(usuario == "admin" && senha == "123456"){
+    window.location = "index.html";
+    logado = 1;
+  }
+
+  if(logado == 0) {
+    alert("Usuário ou senha inválidos!");
+    return false;
+  }
+}
+
+function cadastro(){
+  alert("Cadastro realizado com sucesso!");
+  window.location = "index.html";
+}
