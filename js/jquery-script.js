7ea6d20c2d5c0592fe32e767469f570215ac6e9a
Goetz <matheusgoetz@gmail.com>
2025-02-23 22:15:05 -0300

    Ajuste na estilização da pagina de login, criando novo arquivo css somente para login

--- js/jquery-script.js
+++ js/jquery-script.js
@@ -5,5 +5,11 @@ $(document).ready(function(){
     $('#section-login').slideToggle("slow")
     $('#botao-cadastrar').hide()
   });
+  
+  $('#botao-logar').click(function(){
+    $('#form-cadastrar').slideToggle("slow")
+    $('#section-login').slideToggle("slow")
+    $('#botao-cadastrar').show()
+  });
 
 });
